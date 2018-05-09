f7cb20aea9d21d78355e46de07afc1e1a96812cf
MatejMecka <matej.plavevski+github@gmail.com>
2018-05-09 10:36:50 +0200

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Website Status Monitor
+A Google Assistant Project with the Goal of checking if a website is up or down!
+
+# Requirements
+* Express & Requests
+
+# Installation
+`npm install package.json`
+
+# Getting Started
+Run ` server.js`
---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var http = require('http');
+var https = require('https');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/version', (req, res) => {
+    res.status(200).send("Website Status Monitor. Version 1.0.0");
+});
+
+app.get('/', (req, res) => {
+    res.status(200).send("Hello from APIAI Webhook Integration.");
+});
+
+/* Handling all messenges */
+app.post('/webhook', (req, res) => {
+  console.log(req.body);
+  var regex = /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$/
+  var website = req.body.result.parameters["Websites"]
+  var language = req.body["lang"]
+  //console.log(language)
+  if(website.match(regex)){
+    console.log('nothing to do here')
+  }
+  else{
+    website = website + '.com'
+  }
+  var url = 'https://' + website.toLowerCase()
+  var method = 'https'
+  console.log('website: ' + website)
+  console.log(url)
+  https.get(url, function (response) {
+    console.log(response.statusCode)
+    var message;
+    
+    if(language == "de"){
+      message = 'Webseite is Online Status Code: ' + response.statusCode
+    }
+    else{
+      message = "Website is Accessible. Status Code:" + response.statusCode
+    }
+    
+    res.status(200).json({
+            speech: message,
+            displayText: message,
+            source: 'cloudServiceMonitor'
+    });  
+ 
+      res.status(200).json({
+            speech: message,
+            displayText: message,
+            source: 'cloudServiceMonitor'
+    });  
+
+  }).on('error', function(e) {
+    var message;
+    
+    if(language == "de"){
+      message = 'Webseite ist Offline das ist die Antwort: ' + e
+    }
+    else{
+      message = 'Website is down. This is the response:' + e
+    }
+    
+    res.status(200).json({
+      speech: message,
+      displayText: message,
+      source: 'cloudServiceMonitor'
+    });  
+
+          
+  });
+
+});
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+
