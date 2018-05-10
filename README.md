8097701a9bc115c177940ba0ba59745cbb2896d1
PrettierCI <commits@prettierci.com>
2018-05-10 08:01:47 +0000

    Sync with Prettier

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # Website Status Monitor [![CircleCI](https://circleci.com/gh/MatejMecka/WebsiteStatusMonitor.svg?style=svg)](https://circleci.com/gh/MatejMecka/WebsiteStatusMonitor)
+
 A Google Assistant Project with the Goal of checking if a website is up or down!
 
 # Requirements
+
 * Express & Requests
 
 # Installation
+
 `npm install package.json`
 
 # Getting Started
-Run ` server.js`
+
+Run `server.js`
--- test/test.js
+++ test/test.js
@@ -1,17 +1,20 @@
-var chai = require('chai')
-var action = require('../server')
-var chaiHttp = require('chai-http');
+var chai = require("chai");
+var action = require("../server");
+var chaiHttp = require("chai-http");
 
 var expect = chai.expect;
-chai.use(chaiHttp)
+chai.use(chaiHttp);
 
-describe('Assistant Action', function() {
-	describe('/version', function() {
-		it('responds with status 200', function(done) {
-			chai.request(action).get('/version').end(function(err, res){
-          		expect(res).to.have.status(200);
-          		done();
-        	});
-		});
-	});
+describe("Assistant Action", function() {
+  describe("/version", function() {
+    it("responds with status 200", function(done) {
+      chai
+        .request(action)
+        .get("/version")
+        .end(function(err, res) {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+  });
 });
